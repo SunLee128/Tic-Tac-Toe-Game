996818363cf16ffbed3a1caf9cb3ac637a4e10bf
sunlee <sunlee128@gmail.com>
2019-10-16 16:40:50 +1100

    first day

--- app.js
+++ app.js
@@ -21,12 +21,15 @@ var clickCounter = 0
 
 //handleClick
 var handleClick = function(event){
+
+    // making a move
     if(event.target.textContent ===""){
         if(clickCounter%2 === 0){
             event.target.textContent = markX;
             event.target.classList.add('X')
-            markXId.push(event.target.id)
-            markXId.sort();
+            board[Number(event.target.id[1])] = markX;
+            // markXId.push(event.target.id)
+            // markXId.sort();
             clickCounter++;
             // if (markXId.length=3){
             //     // checkWinsX();
@@ -34,15 +37,48 @@ var handleClick = function(event){
         } else {
             event.target.textContent = markO;
             event.target.classList.add('O')
-            markOId.push(event.target.id);
-            markOId.sort();
+            board[Number(event.target.id[1])] = markO;
+            // markOId.push(event.target.id);
+            // markOId.sort();
             clickCounter++;
             // if (markOId.length=3){
             //     // checkWinsY();
             // }
         }      
     }
+
+
+    // check for wins
+
+    // if (board[wins[0][0]] == 'X' && board[wins[0][1]] == 'X' && board[wins[0][2]] == 'X') {
+    //     console.log('won')
+    // }
+
+
 }
+
+// for(var i=0; i<wins.length; i++){
+//     for(var j=0; i<wins[i].length; j++){
+//         if(board[wins[i][j]] === markX)   
+//     }  
+// }
+
+
+
+switch(board) {
+    case board[0]==="X" && board[1]==="X" && board[2]==="X":
+    case board[3]==="X" && board[4]==="X" && board[5]==="X":
+    case board[6]==="X" && board[7]==="X" && board[8]==="X":
+    case board[0]==="X" && board[3]==="X" && board[6]==="X":
+    case board[1]==="X" && board[4]==="X" && board[7]==="X":
+    case board[2]==="X" && board[5]==="X" && board[8]==="X":
+    case board[0]==="X" && board[4]==="X" && board[8]==="X":
+    case board[2]==="X" && board[4]==="X" && board[6]==="X":
+        alert("Winner X!");
+        break;
+ }
+
+
 
 // var checkWinsX = function(){
 //     var xString = markXId.join()
