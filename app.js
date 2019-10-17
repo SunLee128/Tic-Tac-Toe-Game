10f3d350e417e5bb5fe322b563f424c6a12b02ee
sunlee <sunlee128@gmail.com>
2019-10-17 11:23:45 +1100

    css animation

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var boxes = document.querySelectorAll(".box");
-// var player1Box = document.querySelectorAll(".player1");
-// var player2Box = document.querySelectorAll(".player2");
 var markO = 'O'
 var markX = 'X'
-// var markOId = []
-// var markXId = []
+var msg = document.querySelector(".msg")
 var wins = [
     [0, 1, 2],
 	[3, 4, 5],
@@ -15,119 +12,71 @@ var wins = [
 	[0, 4, 8],
 	[2, 4, 6]
 ]
+var winCaseX = []
+var winCaseY = []
 var winsString = ["012","345","678","036","147","258","048","246"]
 var board = [0,1,2,3,4,5,6,7,8]
 var clickCounter = 0
 
-//handleClick
+//handleClick - making a move
 var handleClick = function(event){
-
-    // making a move
+    // if box is empty
     if(event.target.textContent ===""){
-    
+        // when counter is even number
         if(clickCounter%2 === 0){
             event.target.textContent = markX;
             event.target.classList.add('X')
             board[Number(event.target.id[1])] = markX;
-            // markXId.push(event.target.id)
-            // markXId.sort();
+            msg.textContent = "O's turn now"
             clickCounter++;
-            // if (markXId.length=3){
-            //     // checkWinsX();
-            // }
-        } else {
+        } 
+            //when counter is odd number
+            else {
             event.target.textContent = markO;
             event.target.classList.add('O')
             board[Number(event.target.id[1])] = markO;
-            // markOId.push(event.target.id);
-            // markOId.sort();
             clickCounter++;
-            // if (markOId.length=3){
-            //     // checkWinsY();
-            // }
+            msg.textContent = "X's turn now"
         }      
     }
-    // check for wins
-    for (var i = 0; i < wins.length; i++) {
+    checkWins();
+}
+//check wins against var Wins
+var checkWins = function(){
+    for(var i = 0; i < wins.length; i++) {
         if (board[wins[i][0]] == 'X' && board[wins[i][1]] == 'X' && board[wins[i][2]] == 'X') {
-            alert('X has won');
-            handleReplay()
+            msg.textContent = "X has won!" 
+            winInd1 = board.indexOf(board[wins[i][0]]);
+            winInd2 = board.indexOf(board[wins[i][1]]);
+            winind3 = board.indexOf(board[wins[i][2]]);
+            winCaseX.push(wins[i])
+            highlightWinner();
+            return winCaseX
         } else if (board[wins[i][0]] == 'O' && board[wins[i][1]] == 'O' && board[wins[i][2]] == 'O'){
-            alert('O has won!!');
-            handleReplay()
-        } 
-        
+            msg.textContent = "O has won!"
+            highlightWinner();
+        } else if (clickCounter === 9){
+            msg.textContent = "It is a draw!"
+        }
     }
 }
 
-// for(var i=0; i<wins.length; i++){
-//     for(var j=0; i<wins[i].length; j++){
-//         if(board[wins[i][j]] === markX)   
-//     }  
-// }
-
-// switch(board) {
-//     case board[0]==="X" && board[1]==="X" && board[2]==="X":
-//     case board[3]==="X" && board[4]==="X" && board[5]==="X":
-//     case board[6]==="X" && board[7]==="X" && board[8]==="X":
-//     case board[0]==="X" && board[3]==="X" && board[6]==="X":
-//     case board[1]==="X" && board[4]==="X" && board[7]==="X":
-//     case board[2]==="X" && board[5]==="X" && board[8]==="X":
-//     case board[0]==="X" && board[4]==="X" && board[8]==="X":
-//     case board[2]==="X" && board[4]==="X" && board[6]==="X":
-//         alert("Winner X!");
-//         break;
-//  }
-
-
-
-// var checkWinsX = function(){
-//     var xString = markXId.join()
-//     while(i<winsString.length){
-//         if (winsString[i]===xfirst3){
-//             console.log('Winner is X')
-//             break;
-//         }
-//     }
-// }
-
-
-
-//compare players boxId with winning combination
-// var checkWins = function(wins, array){
-//     var first3Items = array.slice(0,3)
-//     for(var i=0;i<wins.length; i++){
-//         var countMatch = 0;
-//         for(var j=0; j<wins[i].length; j++){
-//            for(var k=0; k=first3Items.length; k++){
-//                if(wins[i][j]===first3Items[k]){
-//                    countMatch++;
-//                    if(countMach === 3){
-//                        break;
-//                     }
-//                 } 
-//             }
-//         }
-//     } 
-//     alert("winner")
-// }
-
+var highlightWinner = function(){
+    //highlight winning boxes and stop the game
+}
 
 //Assign eventListener to boxes
 boxes.forEach(function(box){
     box.addEventListener('click',handleClick);
 })
 
-//replay button----------------------------------------------
 //replay button to reset the board
 var handleReplay = function(event){
     for(var i=0; i<boxes.length; i++){
         boxes[i].textContent = ""
         board[i] = i
+        msg.textContent = "Start!"
     }
 }
 
-//Assing eventListener to replay button
 document.querySelector(".replay").addEventListener('click',handleReplay)
-//------------------------------------------------------------ 
-
---
+++ readme.md
@@ -0,0 +1,29 @@
+### Project #1: Tic Tac Toe
+
+### Link
+(https://sunlee128.github.io/Tic-Tac-Toe-Game/)
+
+
+Your app must:
+
+* **Render a game in the browser**
+* **Switch turns** between more than one player
+* **Design logic for winning** & **visually display which player won**
+* **Include separate HTML / CSS / JavaScript files**
+* Stick with **KISS (Keep It Simple Stupid)** principles
+* Use **Javascript** for **DOM manipulation**
+* **Deploy your game online**, where the rest of the world can access it
+* Use **semantic markup** for HTML and CSS (adhere to best practices)
+
+---
+
+### Necessary Deliverables
+
+
+* **tomorrow** - At least one artefact of either pseudocode, flow diagram, wireframe, mockup or storyboard demonstrating planning - **Due tomorrow** 
+* A **working game, built by you**, hosted somewhere on the internet
+* A **link to your hosted working game** in the URL section of your GitHub repo
+* A **git repository hosted on GitHub**, with a link to your hosted game, and **frequent commits** dating back to the very beginning of the project
+* **A ``readme.md`` file** with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+* Presentation on Friday 2:30pm
+
